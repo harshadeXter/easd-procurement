d46d1c3c98fd7e155b8dea48a27336e70bb2c43d
Saditha <tmsaditha@gmail.com>
2021-10-30 13:44:58 +0530

    Create Dashboard

--- src/main/java/com/esad/procurement/controller/DashboardController.java
+++ src/main/java/com/esad/procurement/controller/DashboardController.java
@@ -1,47 +1,14 @@
+package com.esad.procurement.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
-@RequestMapping(value = "/dashboard")
+@RequestMapping(value = "/")
 public class DashboardController {
-
     @RequestMapping(method = RequestMethod.GET)
     public String get() {
         return "dashboard";
     }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public String onSubmit(HttpServletRequest req) {
-
-        String birthday = req.getParameter("birthday");
-        String color = req.getParameter("color");
-
-        //get the currently-logged-in account:
-        Account account = AccountResolver.INSTANCE.getAccount(req);
-
-        if (account != null) {
-
-            CustomData data = account.getCustomData();
-
-            if (Strings.hasText(birthday)) {
-                data.put("birthday", birthday);
-            } else {
-                data.remove("birthday");
-            }
-
-            if (Strings.hasText(color)) {
-                data.put("color", color);
-            } else {
-                data.remove("color");
-            }
-
-            data.save();
-        }
-
-        return "dashboard";
-    }
-
 }
--- src/main/java/com/esad/procurement/controller/ReturnSupplierInvoiceController.java
+++ src/main/java/com/esad/procurement/controller/ReturnSupplierInvoiceController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
@@ -16,7 +17,7 @@ public class ReturnSupplierInvoiceController {
     @Autowired
     private ReturnSupplierService returnSupplierService;
 
-    @GetMapping("/")
+    @GetMapping("/returnInvoice")
     public String viewHomePage(Model model) {
         return findPaginated(1, "id", "asc", model);
     }
@@ -24,7 +25,7 @@ public class ReturnSupplierInvoiceController {
     @PostMapping("/saveSupplierInvoice")
     public String saveReturnSupplier(@ModelAttribute("return_supplier_invoices") ReturnSupplierInvoice returnSupplierInvoice) {
         returnSupplierService.saveReturnSupplierInvoice(returnSupplierInvoice);
-        return "redirect:/";
+        return "redirect:/returnInvoice";
     }
 
     @GetMapping("/returnSupplier")
@@ -32,22 +33,22 @@ public class ReturnSupplierInvoiceController {
                                 @RequestParam("sortField") String sortField,
                                 @RequestParam("sortDir") String sortDir,
                                 Model model) {
-            int pageSize = 5;
+        int pageSize = 5;
 
-            Page<PurchaseOrderLine> page = returnSupplierService.findPaginated(pageNo, pageSize, sortField, sortDir);
-            List<PurchaseOrderLine> listPurchaseLines = page.getContent();
-            PurchaseOrderLine requestLines = new PurchaseOrderLine();
+        Page<PurchaseOrderLine> page = returnSupplierService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List<PurchaseOrderLine> listPurchaseLines = page.getContent();
+        PurchaseOrderLine requestLines = new PurchaseOrderLine();
 
-            model.addAttribute("requestLines", requestLines);
-            model.addAttribute("currentPage", pageNo);
-            model.addAttribute("totalPages", page.getTotalPages());
-            model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("requestLines", requestLines);
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
 
-            model.addAttribute("sortField", sortField);
-            model.addAttribute("sortDir", sortDir);
-            model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
 
-            model.addAttribute("listPurchaseLines", listPurchaseLines);
-            return "return_sup_invoice";
-        }
+        model.addAttribute("listPurchaseLines", listPurchaseLines);
+        return "return_sup_invoice";
     }
+}
